ec2fdbc87f22795c1196cf0060e47da9e0796b55
Zyncho <zynchokaneko@gmail.com>
2020-07-17 21:53:07 +0200

    | Heroku deployment |

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const colors = require('colors');
 const express = require('express');
 // initialize express
 const server = express();
-
+const port = process.env.PORT || 3000;
 
 /*
 Set up EJS templates
@@ -14,18 +14,23 @@ Set up EJS templates
 server.set('views',__dirname + "/views");
 server.set('view engine','ejs');
 
-
 /*
 set URL paths
 */
 const routes = require('./routes');
 server.use(routes);
+/*
+set assets for Error Page 404
+*/
+server.use(
+  express.static(__dirname + '/views/assets')
+)
 // 404 (any other url request)
 server.get("*",(req,res)=>{res.render('error-page.ejs')})
 
 /*
 Set server to Listen Port
 */
-server.listen(3000, (err,data) => {
+server.listen(port, (err,data) => {
   console.log("SERVER OK on port 3000".underline.brightGreen);
 });
